8635db0579c2faf7c4bd8c749af2897761d76891
Rostja <rasto.dorcak@gmail.com>
2024-11-02 10:56:11 +0100

    start coding

---
+++ src/test/java/com/javaremotecz12/weatherlady/WeatherladyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaremotecz12.weatherlady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherladyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
