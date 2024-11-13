6eb96ec8535af974b5d75b37f1f1623b572f1fb3
Rostja <rasto.dorcak@gmail.com>
2024-11-13 17:03:47 +0100

    changes main.html (processFormVersionTwo) main-form.html (message) WeatherLadyController.java (processFormVersionTwo)

--- src/main/java/com/JavaCZSKRemote/WeatherLady/controller/WeatherLadyController.java
+++ src/main/java/com/JavaCZSKRemote/WeatherLady/controller/WeatherLadyController.java
@@ -1,5 +1,6 @@
 package com.JavaCZSKRemote.WeatherLady.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,18 @@ public class WeatherLadyController {
     @GetMapping("/processForm")
     public String processForm(Model theModel) {
         theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "main";
+    }
+
+    @GetMapping("/processFormVersionTwo")
+    public String processFormVersionTwo(HttpServletRequest request, Model theModel) {
+        String coordinates =request.getParameter("latitude" + " and " + "longitude");
+        coordinates = coordinates.toString();
+        String result = "My coordinates are " + coordinates;
+
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+        theModel.addAttribute("message", result);
 
         return "main";
     }
