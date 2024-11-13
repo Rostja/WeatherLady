dd1642d17e1253ded848f97e4597ea89258d2a19
Rostja <rasto.dorcak@gmail.com>
2024-11-13 15:57:45 +0100

    new main-form.html update WeatherLadyController.java

--- src/main/java/com/JavaCZSKRemote/WeatherLady/controller/WeatherLadyController.java
+++ src/main/java/com/JavaCZSKRemote/WeatherLady/controller/WeatherLadyController.java
@@ -3,9 +3,16 @@ package com.JavaCZSKRemote.WeatherLady.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WeatherLadyController {
+
+    @RequestMapping("/mainForm")
+    public String mainForm(){
+
+        return "main-form";
+    }
 
     @GetMapping("/main")
     public String main(Model theModel) {
