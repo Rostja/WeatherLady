06e329bfdfae45f0416b8bcc8dc1ba70679dc1c9
Rostja <rasto.dorcak@gmail.com>
2024-11-13 20:04:00 +0100

    new Location.java LocationController.java (processLocationForm -> location-confirmation)

--- src/main/java/com/JavaCZSKRemote/WeatherLady/controller/LocationController.java
+++ src/main/java/com/JavaCZSKRemote/WeatherLady/controller/LocationController.java
@@ -4,15 +4,25 @@ import com.JavaCZSKRemote.WeatherLady.model.Location;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LocationController {
     @GetMapping("/showForm")
-    public String showForm(Model theModel){
+    public String showForm(Model theModel) {
 
         Location theLocation = new Location();
-        theModel.addAttribute("location",theLocation);
+        theModel.addAttribute("location", theLocation);
 
         return "location-form";
+    }
+
+    @PostMapping("/processLocationForm")
+    public String processLocationForm(@ModelAttribute("location") Location theLocation) {
+        System.out.println("theLocation: "
+                + theLocation.getLatitude() + " " + theLocation.getLongitude());
+
+        return "location-confirmation";
     }
 }
