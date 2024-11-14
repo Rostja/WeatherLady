8d8c0daf59cb945a306bec4d1ba2742b32ef6614
Rostja <rasto.dorcak@gmail.com>
2024-11-14 12:45:24 +0100

    changes application.properties (languages) location-form.html (tempLanguage) LocationController.java (@Value("${languages}"))

--- src/main/java/com/JavaCZSKRemote/WeatherLady/controller/LocationController.java
+++ src/main/java/com/JavaCZSKRemote/WeatherLady/controller/LocationController.java
@@ -1,20 +1,28 @@
 package com.JavaCZSKRemote.WeatherLady.controller;
 
 import com.JavaCZSKRemote.WeatherLady.model.Location;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class LocationController {
+
+    @Value("${languages}")
+    private List<String> languages;
+
     @GetMapping("/showLocationForm")
     public String showLocationForm(Model theModel) {
 
         Location theLocation = new Location();
         theModel.addAttribute("location", theLocation);
 
+        theModel.addAttribute("languages", languages);
         return "location-form";
     }
 
