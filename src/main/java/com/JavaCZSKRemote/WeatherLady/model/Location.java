260a2e4b59ab5d7aedb002be17e8ed4eca42391e
Rostja <rasto.dorcak@gmail.com>
2024-11-14 14:56:31 +0100

    update validation of Location.java

--- src/main/java/com/JavaCZSKRemote/WeatherLady/model/Location.java
+++ src/main/java/com/JavaCZSKRemote/WeatherLady/model/Location.java
@@ -1,48 +1,25 @@
 package com.JavaCZSKRemote.WeatherLady.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Setter
+@Getter
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Location {
 
+    @NotNull(message="latitude is required")
+    @Size(min=1, message="latitude is required")
     private Double latitude;
+
+    @NotNull(message="longitude is required")
+    @Size(min=1, message="longitude is required")
     private Double longitude;
+
     private TimeType timeType;
     private UnitSystem unitSystem;
 
-    public UnitSystem getUnitSystem() {
-        return unitSystem;
-    }
-
-    public void setUnitSystem(UnitSystem unitSystem) {
-        this.unitSystem = unitSystem;
-    }
-
-    public TimeType getTimeType() {
-        return timeType;
-    }
-
-    public void setTimeType(TimeType timeType) {
-        this.timeType = timeType;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
 }
